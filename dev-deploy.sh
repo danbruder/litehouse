8d862249e510a83935c36274f6a5e4e8a563b9f9
Dan Bruder <danbruder@hey.com>
2026-01-23 10:25:15 -0500

    deploy

---
+++ dev-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+SERVER="${LITEHOUSE_SERVER:-server}"
+
+echo "==> Building static binary..."
+TARGET_CC=x86_64-linux-musl-gcc cargo build --release --target x86_64-unknown-linux-musl
+
+echo "==> Uploading to $SERVER..."
+scp target/x86_64-unknown-linux-musl/release/lh "$SERVER":/tmp/lh
+
+echo "==> Running upgrade on $SERVER..."
+ssh "$SERVER" "sudo /tmp/lh upgrade --from-path /tmp/lh"
+
+echo "==> Done!"
